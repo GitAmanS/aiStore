dd97a1b66b7b94a842f7d0129cd00e5d1b97a103
Kesehet <hamood.siddiqui@gmail.com>
2025-02-03 11:25:49 +0530

    Add context clearing endpoint and improve response formatting in frontend

--- app.py
+++ app.py
@@ -5,19 +5,22 @@ app = Flask(__name__)
 
 @app.route('/request', methods=['POST'])
 def process_request():
-    # write code to compensate for application/x-www-form-urlencoded
     if request.headers['Content-Type'] == 'application/x-www-form-urlencoded':
         data = request.form
     else:
         data = request.get_json()
     response = get_response(data['query'])
     return response
-                    
 
-# A simple / route that loads index.html to test /request
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/context-clear')
+def clear_context():
+    global context
+    context = []
+    return 'Context cleared'
 
 if __name__ == '__main__':
     app.run(debug=True)
